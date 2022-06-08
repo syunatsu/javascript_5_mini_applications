179381161c7f3b91c54010bd50a0aa53367de48f
syunatsu <myf.tyys@gmail.com>
2022-06-08 18:20:43 +0900

    トップページの作成

---
+++ src/js/modules/index.js
@@ -0,0 +1,35 @@
+const nav = document.querySelector('#nav');
+const cover = document.querySelector('#cover');
+const appNames = ['todo', 'typing', 'slide-puzzle', 'memory-card', 'life'];
+
+appNames.forEach((appName) => {
+  const menu = document.createElement('a');
+  // menuにnav-menuクラスを付加
+  menu.classList.add('nav-menu');
+  // menuの文字をappNameに置き換えた
+  menu.textContent = appName.toUpperCase();
+
+  // menuにクリックイベントを付加
+  menu.addEventListener('click', () => {
+    //coverからactiveクラスを除外
+    cover.classList.remove('active');
+    
+    //appElsからactiveクラスを除外(sectionの部分)
+    const appEls = document.querySelectorAll('.app');
+    appEls.forEach((appEl) => {
+      appEl.classList.remove('active');
+    })
+    //appElにactiveクラスを付加する
+    const appE1 = document.getElementById(appName);
+    appE1.classList.add('active');
+
+    //全てのnav-menuを取得し、先に全ての要素からactiveクラスを除外する
+    const navMenus = document.querySelectorAll('.nav-menu');
+    navMenus.forEach((navMenu) => {
+      navMenu.classList.remove('active');
+    })
+    menu.classList.add('active');
+  });
+  //menuをnavの子要素に追加する
+  nav.appendChild(menu);
+});
